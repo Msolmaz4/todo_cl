abc261006ef2d62fcb2ef4a3f8be94065853163b
mustafa <msolmaz83@gmail.com>
2023-11-06 22:05:17 +0100

    fertig

--- src/App.js
+++ src/App.js
@@ -1,53 +1,35 @@
-import React, { useEffect, useState } from 'react';
-import {BsFillMoonStarsFill, BsSunFill} from  'react-icons/bs'
-
-
 import './App.css';
-
+import { useState, useEffect } from 'react';
 import Switch from "react-switch";
+import { BsSunFill, BsFillMoonStarsFill } from 'react-icons/bs'
 import TaskContanier from './components/TaskContainer';
+
 function App() {
 
+  const [tasks, setTasks] = useState([])
+  const [dark, setDark] = useState(true);
+  
+  useEffect(() => {
+    let myTodo = localStorage.getItem('myTodoTasks');
+    if (myTodo) {
+        setTasks(JSON.parse(myTodo))
+    }
+}, [])
 
-   
-  const [tasks ,setTasks] =useState([])
-  const [dark ,setDark] = useState(true)
-
-  useEffect(()=>{
-    const myTodo = localStorage.getItem('myTodaTasks')
-     myTodo ? setTasks(JSON.parse(myTodo)) : console.log('ich habe nicht verbindet')
-
-  },[])
   return (
-    <div className={`${dark ? 'darkMode-App' :'lightMode-App' } App`}>
-     <div className={`${dark ? 'darkMode-App-title-contanier' : ' lightMode-app-title-contanier'} app-title-contanier`}>
-      <h1 className='app-title'> To Do App</h1>
-      <Switch 
-      checked={dark}
-      onChange={()=>setDark(!dark)}
-      uncheckedIcon={
-        <div className='check-sun-btn'>
-        <BsSunFill size={18} /> </div>
-      }
-      checkedIcon={
-        <div className='check-moon-btn'>
-        <BsFillMoonStarsFill size={18}/>
-        </div>
-      }
-      />
-   <TaskContanier 
-     tasks= {tasks} 
-     setTasks = {setTasks} 
-      dark = {dark}/>
-     </div>
-     
-    </div>
+    <div className={`${dark ? 'darkMode-App' : "lightMode-App"} App`}>
+      <div className={`${dark ? 'darkMode-app-title-container' : "lightMode-app-title-container"} app-title-container`}>
+        <h1 className='app-title'>ToDo App</h1>
+      </div>
+      <Switch
+        checked={dark}
+        onChange={() => setDark(!dark)}
+        uncheckedIcon={<div className='check-sun-btn' ><BsSunFill size={18} /></div>}
+        checkedIcon={<div className='check-moon-btn'><BsFillMoonStarsFill size={18} /></div>} />
+
+      <TaskContanier tasks={tasks} setTasks={setTasks} dark={dark} />
+    </div >
   );
 }
 
 export default App;
-// const person = { name: "Alex" };
-// localStorage.setItem("user", person);
-// console.log(localStorage.getItem("user")); // "[object Object]"; not useful!
-// localStorage.setItem("user", JSON.stringify(person));
-// console.log(JSON.parse(localStorage.getItem("user"))); // { name: "Alex" }
