df639f186345aa545d7c6724760c7dceca4ee5bb
mustafa <msolmaz83@gmail.com>
2023-11-06 15:07:35 +0100

    react_stwich

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
+import React, { useState } from 'react';
 
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Switch from "react-switch";
 function App() {
+  const [switched, setSwitched] = useState(false);
+
+  const handleSwitchChange = (checked) => {
+    setSwitched(checked);
+  };
+  
   return (
     <div>
-    hello
-    
+      <h1>React Switch Örneği</h1>
+      <label>
+        Anahtarı Aç/Kapat
+        <Switch
+          onChange={handleSwitchChange}
+          checked={switched}
+        />
+      </label>
+      <p>Anahtar {switched ? 'açık' : 'kapalı'}</p>
     </div>
   );
 }
 
 export default App;
+// const person = { name: "Alex" };
+// localStorage.setItem("user", person);
+// console.log(localStorage.getItem("user")); // "[object Object]"; not useful!
+// localStorage.setItem("user", JSON.stringify(person));
+// console.log(JSON.parse(localStorage.getItem("user"))); // { name: "Alex" }
